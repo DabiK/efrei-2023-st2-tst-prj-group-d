98502203ac269217a594fe6ce056f54bd33b8cdf
unknown <nathan.manaranche@gmail.com>
2023-01-03 11:58:22 +0100

    Updating base-page

--- tests/pages/base-page.js
+++ tests/pages/base-page.js
@@ -1,6 +1,18 @@
+
+
 export class BasePage {
-
-    page;
     locators;
+    page;
 
+    constructor(page,locatorConfiguration){
+        this.page = page;
+        this.locators = {}
+        Object.keys(locatorConfiguration).forEach(key => {
+            this.locators[key] = page.locator(locatorConfiguration[key]);
+        })
+    }
+
+    async wait(time){
+        await this.page.waitForTimeout(time);
+    }
 }
--- tests/pages/home-page.js
+++ tests/pages/home-page.js
@@ -2,16 +2,16 @@ import { BasePage } from "./base-page";
 
 export class HomePage extends BasePage {
 
-    selectors = [
+    static HOME_PAGE_SELECTORS = [
         'a[href="/reset_db"]', // Lien reset dbb
     ]
 
     constructor(page) {
-        super(page, this.selectors);
+        super(page, HomePage.HOME_PAGE_SELECTORS);
     }
 
     async clickOnReset() {
-        const resetLink = this.locators[''];
+        const resetLink = this.locators['a[href="/reset_db"]'];
         resetLink.click();
     }
 }
